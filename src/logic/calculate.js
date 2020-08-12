51788425e9f4cfa1ea122be3bb419675140ecd39
Alexandru <rammazzoti2000@gmail.com>
2020-08-12 20:44:47 +0200

    Changed calculate if conditionals to switch method due to ambiguous buggy code

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,121 +1,46 @@
-// import operate from './operate';
-//
-// function calculate(dataObj, buttonName) {
-//   let { total, next, operation } = dataObj;
-//
-//   if (buttonName === '+/-') {
-//     if (next) {
-//       next = operate(next, '-1', 'X');
-//     } else if (total) {
-//       total = operate(total, '-1', 'X');
-//     }
-//   }
-//
-//   if (buttonName === '=') {
-//     if (next) {
-//       total = operate(total, next, operation);
-//       next = undefined;
-//       operation = undefined;
-//     }
-//   }
-//
-//   if (buttonName === '%') {
-//     if (next) {
-//       total = operate(total, next, operation);
-//       total = operate(total, '100', '÷');
-//       next = undefined;
-//       operation = undefined;
-//     } else {
-//       total = operate(total, '100', '÷');
-//     }
-//   }
-//
-//   if (buttonName === 'AC') {
-//     if (next) {
-//       next = '0';
-//     } else if (operation) {
-//       operation = undefined;
-//     } else {
-//       total = '0';
-//     }
-//   }
-//
-//   if (buttonName === '.') {
-//     if (next) {
-//       if (!next.includes('.')) {
-//         next += '.';
-//       }
-//     } else if (operation) {
-//       next = '0.';
-//     } else if (!total.includes('.')) {
-//       total += '.';
-//     }
-//   }
-//
-//   if (buttonName === '0' || buttonName === '1' || buttonName === '2'
-//    || buttonName === '3' || buttonName === '4' || buttonName === '5'
-//    || buttonName === '6' || buttonName === '7' || buttonName === '8'
-//    || buttonName === '9') {
-//     if (next) {
-//       next += buttonName;
-//     } else if (operation) {
-//       next = buttonName;
-//     } else if (total === '0') {
-//       total = buttonName;
-//     } else {
-//       total += buttonName;
-//     }
-//   }
-//
-//   return { total, next, operation };
-// }
-//
-// export default calculate;
-
-
 import operate from './operate';
 
-function calculate(calculatorObj, buttonName) {
-  let { total, next, operation } = calculatorObj;
+function calculate(dataObj, buttonName) {
+  let { total, next, operation } = dataObj;
 
   switch (buttonName) {
     case '%':
       if (next) {
         total = operate(total, next, operation);
         total = operate(total, '100', '÷');
-        next = undefined;
-        operation = undefined;
+        next = null;
+        operation = null;
       } else {
         total = operate(total, '100', '÷');
       }
       break;
+
     case '+/-':
       if (next) {
         next = operate(next, '-1', 'X');
       } else {
         total = operate(total, '-1', 'X');
       }
-
       break;
+
     case '=':
-      // don't do anything if the only value is the total
       if (next) {
         total = operate(total, next, operation);
-        next = undefined;
-        operation = undefined;
+        next = null;
+        operation = null;
       }
-
       break;
+
     case 'AC':
       if (next) {
         next = '0';
       } else if (operation) {
-        operation = undefined;
+        operation = null;
       } else {
         total = '0';
       }
-
       break;
+
     case '.':
       if (next) {
         if (!next.includes('.')) {
@@ -126,8 +51,8 @@ function calculate(calculatorObj, buttonName) {
       } else if (!total.includes('.')) {
         total += '.';
       }
-
       break;
+
     case '0':
     case '1':
     case '2':
@@ -151,7 +76,7 @@ function calculate(calculatorObj, buttonName) {
       break;
     default:
       total = operate(total, next, operation);
-      next = undefined;
+      next = null;
       operation = buttonName;
       break;
   }
--- src/test/calculate.test.js
+++ src/test/calculate.test.js
@@ -1,147 +1,147 @@
 import calculate from '../logic/calculate';
 
-function buildCalculator(total, next = undefined, operation = undefined) {
+function calc(total, next = null, operation = null) {
   return { total, next, operation };
 }
 
-describe('addition, subtraction, multiplication, or division', () => {
-  describe('pending operation before continuing', () => {
-    test('calculates and builds new calculator with pending operation', () => {
-      const numberOne = '1';
-      const numberTwo = '2';
+describe('\'+/-\', \'X\', \'÷\' operations', () => {
+  describe('chaining multiple operations', () => {
+    test('calculation with unconcluded operation', () => {
+      const numOne = '-1';
+      const numTwo = '2';
       let calculator;
-      let given;
-      let expected;
+      let input;
+      let output;
 
-      // // 1 + 2 + -> 3 + ?
-      // calculator = buildCalculator(numberOne, numberTwo, '+');
-      // given = calculate(calculator, '+');
-      // expected = buildCalculator('3', undefined, '+');
-      // expect(given).toEqual(expected);
+      // 1 + 2 + -> 3 + ?
+      calculator = calc(numOne, numTwo, '+');
+      input = calculate(calculator, '+');
+      output = calc('1', null, '+');
+      expect(input).toEqual(output);
 
       // 1 - 2 X -> -1 X ?
-      calculator = buildCalculator(numberOne, numberTwo, '-');
-      given = calculate(calculator, 'X');
-      expected = buildCalculator('-1', undefined, 'X');
-      expect(given).toEqual(expected);
+      calculator = calc(numOne, numTwo, '-');
+      input = calculate(calculator, 'X');
+      output = calc('-3', null, 'X');
+      expect(input).toEqual(output);
 
       // 1 X 2 ÷ -> 2 ÷ ?
-      calculator = buildCalculator(numberOne, numberTwo, 'X');
-      given = calculate(calculator, '÷');
-      expected = buildCalculator('2', undefined, '÷');
-      expect(given).toEqual(expected);
+      calculator = calc(numOne, numTwo, 'X');
+      input = calculate(calculator, '÷');
+      output = calc('-2', null, '÷');
+      expect(input).toEqual(output);
 
       // 1 + 2 - -> 3 - ?
-      calculator = buildCalculator(numberOne, numberTwo, '+');
-      given = calculate(calculator, '-');
-      expected = buildCalculator('3', undefined, '-');
-      expect(given).toEqual(expected);
+      calculator = calc(numOne, numTwo, '+');
+      input = calculate(calculator, '-');
+      output = calc('1', null, '-');
+      expect(input).toEqual(output);
     });
   });
 });
 
-describe('percents (%)', () => {
-  describe('no next number provided', () => {
-    test('it divides the total by 100', () => {
+describe('mod \'%\' operator', () => {
+  describe('next number is null', () => {
+    test('total is devided by 100', () => {
       // 5 % ? -> 0.05 ? ?
-      const calculator = buildCalculator('5', undefined, undefined);
-      const given = calculate(calculator, '%');
-      const expected = buildCalculator('0.05', undefined, undefined);
-      expect(given).toEqual(expected);
+      const calculator = calc('5', null, null);
+      const input = calculate(calculator, '%');
+      const output = calc('0.05', null, null);
+      expect(input).toEqual(output);
     });
   });
 
-  describe('pending operation', () => {
-    test('it performs the pending operation and divides by 100', () => {
+  describe('unconcluded operation', () => {
+    test('it performs the unconcluded operation and divides by 100', () => {
       // 5 + 19 % -> 0.24 ? ?
-      const calculator = buildCalculator('5', '19', '+');
-      const given = calculate(calculator, '%');
-      const expected = buildCalculator('0.24', undefined, undefined);
-      expect(given).toEqual(expected);
+      const calculator = calc('5', '19', '+');
+      const input = calculate(calculator, '%');
+      const output = calc('0.24', null, null);
+      expect(input).toEqual(output);
     });
   });
 });
 
-describe('swap sign (+/-)', () => {
-  describe('only total is available', () => {
-    test('it swaps the sign of total', () => {
+describe('\'+/-\' swap operators', () => {
+  describe('total the only available', () => {
+    test('total swaps sign', () => {
       // 23 ? +/- -> -23 ? ?
-      const calculator = buildCalculator('23', undefined, undefined);
-      const given = calculate(calculator, '+/-');
-      const expected = buildCalculator('-23', undefined, undefined);
-      expect(given).toEqual(expected);
+      const calculator = calc('23', null, null);
+      const input = calculate(calculator, '+/-');
+      const output = calc('-23', null, null);
+      expect(input).toEqual(output);
     });
   });
 
-  describe('there is a pending operation', () => {
-    test('it swaps the sign of the second operand', () => {
+  describe('there is an unconcluded operation', () => {
+    test('second operand swaps the sign', () => {
       // 5 X 99 +/- -> 5 X (-99)
-      const calculator = buildCalculator('5', '99', 'X');
-      const given = calculate(calculator, '+/-');
-      const expected = buildCalculator('5', '-99', 'X');
-      expect(given).toEqual(expected);
+      const calculator = calc('5', '99', 'X');
+      const input = calculate(calculator, '+/-');
+      const output = calc('5', '-99', 'X');
+      expect(input).toEqual(output);
     });
   });
 });
 
-describe('equal sign (=)', () => {
-  describe('there is a pending operation', () => {
+describe('\'=\' operator', () => {
+  describe('there is a unconcluded operation', () => {
     test('it updates the total and resets the other properties', () => {
       // 87 + (-34) = -> 53 ? ?
-      const calculator = buildCalculator('87', '-34', '+');
-      const given = calculate(calculator, '=');
-      const expected = buildCalculator('53', undefined, undefined);
-      expect(given).toEqual(expected);
+      const calculator = calc('87', '-34', '+');
+      const input = calculate(calculator, '=');
+      const output = calc('53', null, null);
+      expect(input).toEqual(output);
     });
   });
 
   describe('only total is available', () => {
-    test('it sets the total to itself', () => {
+    test('total assinged to own value', () => {
       // 66 ? ? = -> 66 ? ?
-      const calculator = buildCalculator('66', undefined, undefined);
-      const given = calculate(calculator, '=');
-      expect(given).toEqual(calculator);
+      const calculator = calc('66', null, null);
+      const input = calculate(calculator, '=');
+      expect(input).toEqual(calculator);
     });
   });
 });
 
-describe('digits', () => {
+describe('numeric keys', () => {
   describe('operation is present', () => {
     test('it modifies next to be the sequence of digits passed', () => {
       let calculator;
-      let given;
-      let expected;
+      let input;
+      let output;
 
       // 0 + ? 5 -> 0 + 5
-      calculator = buildCalculator('0', undefined, '+');
-      given = calculate(calculator, '5');
-      expected = buildCalculator('0', '5', '+');
-      expect(given).toEqual(expected);
+      calculator = calc('0', null, '+');
+      input = calculate(calculator, '5');
+      output = calc('0', '5', '+');
+      expect(input).toEqual(output);
 
       // 17 X ? 9 -> 17 X 9 8 -> 17 X 98
-      calculator = buildCalculator('17', undefined, 'X');
-      given = calculate(calculator, '9');
-      given = calculate(given, '8');
-      expected = buildCalculator('17', '98', 'X');
-      expect(given).toEqual(expected);
+      calculator = calc('17', null, 'X');
+      input = calculate(calculator, '9');
+      input = calculate(input, '8');
+      output = calc('17', '98', 'X');
+      expect(input).toEqual(output);
     });
   });
 
-  describe('only total is present', () => {
-    describe('total is zero', () => {
+  describe('total only assigned', () => {
+    describe('total has value zero', () => {
       // 0 ? ? 7 -> 7 ? ?
-      const calculator = buildCalculator('0', undefined, undefined);
-      const given = calculate(calculator, '7');
-      const expected = buildCalculator('7', undefined, undefined);
-      expect(given).toEqual(expected);
+      const calculator = calc('0', null, null);
+      const input = calculate(calculator, '7');
+      const output = calc('7', null, null);
+      expect(input).toEqual(output);
     });
 
-    describe('total is not zero', () => {
+    describe('total has value other than zero', () => {
       // 7 ? ? 4 -> 74 ? ?
-      const calculator = buildCalculator('7', undefined, undefined);
-      const given = calculate(calculator, '4');
-      const expected = buildCalculator('74', undefined, undefined);
-      expect(given).toEqual(expected);
+      const calculator = calc('7', null, null);
+      const input = calculate(calculator, '4');
+      const output = calc('74', null, null);
+      expect(input).toEqual(output);
     });
   });
 });
@@ -151,27 +151,27 @@ describe('decimal point', () => {
     describe('there is a next number', () => {
       test('it places a decimal point at the end of the next number', () => {
         // 5 - 3 . -> 5 - 3.
-        const calculator = buildCalculator('5', '3', '-');
-        const given = calculate(calculator, '.');
-        const expected = buildCalculator('5', '3.', '-');
-        expect(given).toEqual(expected);
+        const calculator = calc('5', '3', '-');
+        const input = calculate(calculator, '.');
+        const output = calc('5', '3.', '-');
+        expect(input).toEqual(output);
       });
 
       test('it does nothing if there is a decimal point already', () => {
         // 5 - 3. . -> 5 - 3.
-        const calculator = buildCalculator('5', '3.', '-');
-        const given = calculate(calculator, '.');
-        expect(given).toEqual(calculator);
+        const calculator = calc('5', '3.', '-');
+        const input = calculate(calculator, '.');
+        expect(input).toEqual(calculator);
       });
     });
 
     describe('there is no next number', () => {
       test('it creates 0. as the next number', () => {
         // 5 - ? . -> 5 - 0.
-        const calculator = buildCalculator('5', undefined, '-');
-        const given = calculate(calculator, '.');
-        const expected = buildCalculator('5', '0.', '-');
-        expect(given).toEqual(expected);
+        const calculator = calc('5', null, '-');
+        const input = calculate(calculator, '.');
+        const output = calc('5', '0.', '-');
+        expect(input).toEqual(output);
       });
     });
   });
@@ -179,17 +179,17 @@ describe('decimal point', () => {
   describe('only total is present', () => {
     test('it places a decimal point at the end of the total', () => {
       // 99 ? ? . -> 99. ? ?
-      const calculator = buildCalculator('99');
-      const given = calculate(calculator, '.');
-      const expected = buildCalculator('99.');
-      expect(given).toEqual(expected);
+      const calculator = calc('99');
+      const input = calculate(calculator, '.');
+      const output = calc('99.');
+      expect(input).toEqual(output);
     });
 
     test('it does nothing if there is a decimal point already', () => {
       // 99. ? ? . -> 99. ? ?
-      const calculator = buildCalculator('99.');
-      const given = calculate(calculator, '.');
-      expect(given).toEqual(calculator);
+      const calculator = calc('99.');
+      const input = calculate(calculator, '.');
+      expect(input).toEqual(calculator);
     });
   });
 });
@@ -198,30 +198,30 @@ describe('AC', () => {
   describe('there is a next number', () => {
     test('it sets the next number to zero', () => {
       // 65 + 23 AC -> 65 + 0
-      const calculator = buildCalculator('65', '23', '+');
-      const given = calculate(calculator, 'AC');
-      const expected = buildCalculator('65', '0', '+');
-      expect(given).toEqual(expected);
+      const calculator = calc('65', '23', '+');
+      const input = calculate(calculator, 'AC');
+      const output = calc('65', '0', '+');
+      expect(input).toEqual(output);
     });
   });
 
   describe('there is no next number, but there is an operation', () => {
     test('it undefines the operation', () => {
       // 87 X ? AC -> 87 ? ?
-      const calculator = buildCalculator('87', undefined, 'X');
-      const given = calculate(calculator, 'AC');
-      const expected = buildCalculator('87', undefined, undefined);
-      expect(given).toEqual(expected);
+      const calculator = calc('87', null, 'X');
+      const input = calculate(calculator, 'AC');
+      const output = calc('87', null, null);
+      expect(input).toEqual(output);
     });
   });
 
   describe('there is only total', () => {
     test('it sets total to zero', () => {
       // 76 ? ? AC -> 0 ? ?
-      const calculator = buildCalculator('76');
-      const given = calculate(calculator, 'AC');
-      const expected = buildCalculator('0');
-      expect(given).toEqual(expected);
+      const calculator = calc('76');
+      const input = calculate(calculator, 'AC');
+      const output = calc('0');
+      expect(input).toEqual(output);
     });
   });
 });
