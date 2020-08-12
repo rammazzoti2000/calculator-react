1971ba68ca0692b05c0b0f847d969fd90703a886
Alexandru <rammazzoti2000@gmail.com>
2020-08-12 14:54:19 +0200

    Implement the calculate.js module

---
+++ src/logic/calculate.js
@@ -0,0 +1,11 @@
+import operate from './operate';
+
+function calculate(dataObj, buttonName) {
+  let { total, next, operation } = dataObj;
+
+  // if (buttonName === '+/-') {
+  //   if (next) {
+  //     next = operate()
+  //   }
+  // }
+}
