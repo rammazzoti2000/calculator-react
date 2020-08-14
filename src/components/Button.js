a9a7751d560764659d344efe1e165fe6f24d84b0
Alexandru <rammazzoti2000@gmail.com>
2020-08-14 11:56:00 +0200

    Implement handleClick function to the Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,12 +3,19 @@ import PropTypes from 'prop-types';
 
 function Button(props) {
   const {
-    button, wide, color, handleClick,
+    button, wide, color, clickHandler,
   } = props;
   const buttonClass = `Button ${wide ? 'doubleButton' : ''} ${color === 'gray' ? 'grayKey' : 'orangeKey'}`;
+  const handleClick = name => clickHandler(name);
 
   return (
-    <button type="button" className={buttonClass} onClick={() => handleClick(button)}>
+    <button
+      type="button"
+      className={buttonClass}
+      onClick={() => {
+        handleClick({ button });
+      }}
+    >
       { button }
     </button>
   );
